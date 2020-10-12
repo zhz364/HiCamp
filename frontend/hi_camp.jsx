51997a6eb60f192f113be1ba9cdaeb9617894ce6
zhz364 <zhz364@ucsd.edu>
2020-10-12 14:54:21 -0700

    Setup frontend folder

--- frontend/hi_camp.jsx
+++ frontend/hi_camp.jsx
@@ -1,1 +1,21 @@
-<h1>Hi-camp</h1>
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+
+// test import
+import * as SessionUtil from "./util/session_api_util";
+import { login, logout, signUp } from "./actions/session_actions";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    let preloadedState = undefined;
+    const store = configureStore(preloadedState);
+    ReactDOM.render(<h1>Welcome to HiCamp</h1>, root);
+
+    // Test Section
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.signUp = signUp;
+    window.login = login;
+    window.logout = logout;
+});
