a22d8380781175e0165abd77f832814d07c35aa1
zhz364 <zhz364@ucsd.edu>
2020-10-19 11:08:10 -0700

    Add campsite attach association

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -2,4 +2,5 @@ class Campsite < ApplicationRecord
     validates :title, uniqueness:true
     validates :title, :address, :longitude, :latitude, presence: true
     
+    has_one_attached :photo
 end
