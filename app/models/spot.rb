b2386911a6d9a5f86fdce0738fc19428a96fa256
zhz364 <zhz364@ucsd.edu>
2020-10-19 21:18:06 -0700

    Delete association for spot

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,5 +2,9 @@ class Spot < ApplicationRecord
     validates :name, uniqueness:true
     validates :name, :longitude, :latitude, :camp_type, :img_url, :state_location,:host_id, :price, :capacity, presence: true
 
+    has_many_attached :photos
     
+    
+        
+        
 end
--- app/views/api/spots/index.json.jbuilder
+++ app/views/api/spots/index.json.jbuilder
@@ -1,5 +1,6 @@
 @spots.each do |spot|
     json.set! spot.id do
       json.extract! spot, :id,:name, :camp_type, :longitude,:latitude,:price,:capacity,:img_url, :description, :state_location
+      json.photoUrls spot.photos.map { |file| url_for(file) }
     end
 end
