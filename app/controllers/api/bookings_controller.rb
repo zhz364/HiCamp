f857487059430806e66d1df42f71f800330448df
zhz364 <zhz364@ucsd.edu>
2020-10-21 15:18:21 -0700

    Update booking controller

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,15 +1,26 @@
 class Api::BookingsController < ApplicationController
     
     def index
-
+        @bookings = Booking.where(user_id: params[:user_id])
     end
 
     def create
-
+        @booking = Booking.new(booking_params)
+        if @booking.save
+            render "api/bookings/index"
+        else
+            render json: @booking.errors.full_messages, status:401
     end
 
     def destroy
+        @booking = Booking.find(params[:id])
+        @booking.destroy
 
+    end
+
+    private
+    def booking_params
+        params.require(:booking).permit(:user_id, :spot_id,:checkin_date, :checkout_date,:nums_guest,:total_price)
     end
     
 end
