7344147d7b6f2bef2c408e1fd931806e4fe88b71
zhz364 <zhz364@ucsd.edu>
2020-11-26 09:06:23 -0800

    Add review frontend

---
+++ frontend/actions/review_actions.js
@@ -0,0 +1,51 @@
+import * as reviewUtil from '../util/review_api_util';
+
+export const RECEIVE_ALL_REVIEWS = 'RECEIVE_ALL_REVIEWS';
+export const RECEIVE_REVIEW = 'RECEIVE_REVIEW'; 
+export const REMOVE_REVIEW = 'REMOVE_REVIEW';
+export const RECEIVE_REVIEW_ERRORS = 'RECEIVE_REVIEW_ERRORS';
+export const CLEAR_REVIEW_ERRORS = 'CLEAR_REVIEW_ERRORS'; 
+
+const receiveAllReviews = (reviews) => ({
+    type: RECEIVE_ALL_REVIEWS,
+    reviews
+});
+
+const receiveReview = (review) => ({
+    type: RECEIVE_REVIEW,
+    review
+});
+
+const removeReview = (reviewId) => ({
+    type: REMOVE_REVIEW,
+    reviewId
+});
+
+const receiveErrors = (errors) => ({
+    type: RECEIVE_REVIEW_ERRORS,
+    errors
+});
+
+export const clearErrors = () => ({
+    type: CLEAR_REVIEW_ERRORS
+});
+
+export const fetchReviews = (spotId) => (dispatch)=> {
+    return reviewAPIUtil.fetchReviews(spotId)
+        .then((reviews) => dispatch(receiveAllReviews(reviews)), (errors) => dispatch(receiveErrors(errors.responseJSON)));
+}
+
+export const createReview = (review) => (dispatch) => {
+    reviewUtil.createReview(review)
+        .then((review) => dispatch(receiveReview(review)), (errors) => dispatch(receiveErrors(errors.responseJSON)));
+}
+
+export const updateReview = (review) => (dispatch) => { 
+    reviewUtil.updateReview(review)
+        .then((review) => dispatch(receiveReview(review)), (errors) => dispatch(receiveErrors(errors.responseJSON)));
+}
+
+export const deleteReview = (reviewId) => (dispatch) => {
+    reviewUtil.deleteReview(reviewId)
+        .then((review) => dispatch(removeReview(review.id)), (errors) => dispatch(receiveErrors(errors.responseJSON)));
+}
---
+++ frontend/reducers/reviews_reducer.js
@@ -0,0 +1,25 @@
+import { RECEIVE_ALL_REVIEWS, RECEIVE_REVIEW, REMOVE_REVIEW,RECEIVE_REVIEW_ERRORS, CLEAR_REVIEW_ERRORS } from '../actions/review_actions';
+
+const reviewsReducer = (oldState = {}, action) => {
+    Object.freeze(oldState);
+    switch (action.type) {
+        case RECEIVE_ALL_REVIEWS:
+            return action.reviews;
+        case RECEIVE_REVIEW:
+            return Object.assign({}, oldState, {[action.review.id]: action.review});
+        case REMOVE_REVIEW:
+            const newState = Object.assign({}, oldState);
+            delete newState[action.reviewId];
+            return newState;
+        case RECEIVE_REVIEW_ERRORS:
+            return action.errors;
+        case CLEAR_REVIEW_ERRORS:
+            return [];
+        default: 
+            return state; 
+        default:
+            return oldState;
+    }
+};
+
+export default reviewsReducer; 
