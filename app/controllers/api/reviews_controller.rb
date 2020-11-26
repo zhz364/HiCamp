95ed1e9f7e731cc01da0bcb2767a51a4a4224d3d
zhz364 <zhz364@ucsd.edu>
2020-11-25 16:43:25 -0800

    Add review controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,3 +1,42 @@
 class Api::ReviewsController < ApplicationController
-    
+    def index
+        @reviews = Review.where(spot_id: params[:spot_id])
+        if @reviews.length > 0
+            render :index
+        else 
+            render json: {}
+        end 
+    end 
+
+    def create
+        @review = Review.new(reviewParams)
+        if @review.save
+            render :show
+        else 
+            render json: @review.errors.full_messages, status: 400
+        end 
+    end 
+
+    def update
+        @review = Review.find(params[:id])
+        if @review.update_attributes(reviewParams)
+            render :show
+        else 
+            render json: @review.errors.full_messages, status: 400
+        end 
+    end 
+
+    def destroy
+        @review = Review.find(params[:id]) 
+        if @review.destroy
+            render :show
+        else
+            render json: @review.errors.full_messages, status: 404
+        end
+    end 
+
+    private
+    def reviewParams
+        params.require(:review).permit(:author_id, :spot_id, :title, :body)
+    end 
 end
